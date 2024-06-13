c6968f6cc1d041fa8fa87291e9ab47514660f13d
Darlan Carvalho <137117083+josedarlancarvalho@users.noreply.github.com>
2024-06-13 15:10:54 -0300

    Add files via upload

---
+++ python 02/indentacao_e_blocos.py
@@ -0,0 +1,12 @@
+# o python é uma linguaguem que precisa de indentacao para que o codigo possa rodar.
+def sacar(valor):
+    saldo = 8400 
+    valor = 5200
+
+    if saldo >= valor:
+        print("valor sacado")
+
+        print ("obrigado por ser nosso cliente")  
+    else:
+        print(".")
+sacar (100)
